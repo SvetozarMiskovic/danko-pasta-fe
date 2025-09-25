5c09b98f23e0c2b8e54b2d89e6dbea9cdb5e5993
SvetozarMiskovic <svetomiskovic98@gmail.com>
2025-09-25 20:03:12 +0200

    added encountered-problems.md file

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -2,6 +2,7 @@ import 'i18next';
 import en from '../locales/en/translation.json';
 
 declare module 'i18next' {
+  
   interface CustomTypeOptions {
     defaultNS: 'translation';
     resources: {
